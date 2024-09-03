71bfbeea698f1d655bdb78a0af7242c94feb7da8
gayatri-v <gayatrishende1205@gmail.com>
2024-09-03 14:14:03 +0530

    env setup

--- app.py
+++ app.py
@@ -2,18 +2,23 @@ from flask import Flask,render_template,url_for
 import requests
 from flask import request as req
 
+with open('.env') as f:
+    key = f.readline()
+
+
 app=Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 def Index():
    
-    
+    # print(key)
     return render_template('index.html')
 
 @app.route("/Summarize",methods=['GET','POST'])
 def Summarize():
     if req.method=="POST":
+
         API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-        headers = {"Authorization": "Bearer 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"}
+        headers = {"Authorization": f"Bearer {key}"}
         
         print(req.form["maxL"])
         maxL = int(req.form["maxL"])
